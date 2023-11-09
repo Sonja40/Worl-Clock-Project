8cec5d514a2d71bc2c2dc36c072596db604122e2
Sonja40 <137377644+Sonja40@users.noreply.github.com>
2023-11-09 12:54:27 +0100

    set interval

--- src/app.js
+++ src/app.js
@@ -1,41 +1,45 @@
-//Paris
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-let parisTimeElement = parisElement.querySelector(".time");
-let parisTime = moment().tz("Europe/Paris");
+function updateTime() {
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
-parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
-//Johannesburg
-let johannesburgElement = document.querySelector("#johannesburg");
-let johannesburgDateElement = johannesburgElement.querySelector(".date");
-let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-let johannesburgTime = moment().tz("Africa/Johannesburg");
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
 
-johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-johannesburgTimeElement.innerHTML = johannesburgTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-//Hong-Kong
-let hongkongElement = document.querySelector("#hong-kong");
-let hongkongDateElement = hongkongElement.querySelector(".date");
-let hongkongTimeElement = hongkongElement.querySelector(".time");
-let hongkongTime = moment().tz("Asia/Hong_Kong");
+  //Hong-Kong
+  let hongkongElement = document.querySelector("#hong-kong");
+  let hongkongDateElement = hongkongElement.querySelector(".date");
+  let hongkongTimeElement = hongkongElement.querySelector(".time");
+  let hongkongTime = moment().tz("Asia/Hong_Kong");
 
-hongkongDateElement.innerHTML = hongkongTime.format("MMMM Do YYYY");
-hongkongTimeElement.innerHTML = hongkongTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  hongkongDateElement.innerHTML = hongkongTime.format("MMMM Do YYYY");
+  hongkongTimeElement.innerHTML = hongkongTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-//New York
-let newyorkElement = document.querySelector("#new-york");
-let newyorkDateElement = newyorkElement.querySelector(".date");
-let newyorkTimeElement = newyorkElement.querySelector(".time");
-let newyorkTime = moment().tz("America/New_York");
+  //New York
+  let newyorkElement = document.querySelector("#new-york");
+  let newyorkDateElement = newyorkElement.querySelector(".date");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
+  let newyorkTime = moment().tz("America/New_York");
 
-newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
-newyorkTimeElement.innerHTML = newyorkTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = newyorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
